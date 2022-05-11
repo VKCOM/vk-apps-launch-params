54e3382e70968de88f05ce5c8044311100755576
Vladislav Kibenko <wolfram.deus@gmail.com>
2022-05-11 21:52:41 +0500

    Убрать привязку к параметрам, начинающимся с "vk_"

--- examples/golang.go
+++ examples/golang.go
@@ -8,9 +8,7 @@ import (
 	"fmt"
 	"net/url"
 	"sort"
-	"strconv"
 	"strings"
-	"time"
 )
 
 type queryParameter struct {
@@ -18,18 +16,7 @@ type queryParameter struct {
 	Value string
 }
 
-type LaunchParams struct {
-	// Идентификатор пользователя.
-	UserId int
-	// Идентификатор приложения.
-	AppId int
-}
-
-// Ошибки, возвращаемые VerifyLaunchParams.
-const (
-	SignInvalid = "подпись невалидна"
-	Expired     = "срок действия подписи истёк"
-)
+const SignInvalid = "подпись невалидна"
 
 func main() {
 	const (
@@ -37,11 +24,7 @@ func main() {
 		clientSecret = "wvl68m4dR1UpLrVRli"
 	)
 
-	// Обратите внимание на то, что в этом примере expiresIn равен 0. При
-	// разработке рекомендуется передавать значение отличное от 0. Почему
-	// передавать 0 не стоит описано в пояснении к параметру expiresIn функции
-	// VerifyLaunchParams.
-	if _, err := VerifyLaunchParams(clientUrl, clientSecret, 0); err == nil {
+	if err := VerifyLaunchParams(clientUrl, clientSecret); err == nil {
 		fmt.Println("ok")
 	} else {
 		fmt.Println("fail")
@@ -57,18 +40,7 @@ func main() {
 // полного url (window.location.toString()).
 //
 // secretKey - секретный ключ мини-приложения.
-//
-// expiresIn - срок жизни параметров запуска с момента их выдачи. В случае
-// передачи значения 0, срок действия жизни параметров не проверяется.
-// Использование значения 0 крайне не рекомендуется ввиду того, что в случае
-// кражи параметров запуска пользователя, у злоумышленника всегда будут
-// действующие данные, которые позволяет ему представляться другим
-// пользователем.
-func VerifyLaunchParams(
-	querySearch string,
-	secretKey string,
-	expiresIn int,
-) (*LaunchParams, error) {
+func VerifyLaunchParams(querySearch string, secretKey string) error {
 	var searchIndex = strings.Index(querySearch, "?")
 
 	// Необходимо удалить всё, что находится до search части в случае, если
@@ -78,8 +50,6 @@ func VerifyLaunchParams(
 	}
 
 	var (
-		// Идентификатор приложения.
-		appId int
 		// Отфильтрованные параметры запуска. Мы используем именно
 		// слайс по той причине, что позже нам будет необходимым этот слайс
 		// отсортировать по возрастанию ключа параметра.
@@ -87,10 +57,6 @@ func VerifyLaunchParams(
 		// Подпись, которая была сгенерирована сервером ВКонтакте и основана на
 		// параметрах из query.
 		sign string
-		// Дата, когда параметры запуска были созданы.
-		timestamp int
-		// Идентификатор пользователя.
-		userId int
 	)
 
 	// Разделяем параметры запуска на вхождения, разделенные знаком "&".
@@ -102,20 +68,6 @@ func VerifyLaunchParams(
 		// Мы обрабатываем только те ключи, которые начинаются с префикса "vk_".
 		// Все остальные ключи в создании подписи не участвуют.
 		if strings.HasPrefix(key, "vk_") {
-			switch key {
-			case "vk_ts": // дата выдачи параметров запуска.
-			case "vk_app_id": // идентификатор приложения.
-			case "vk_user_id": // идентификатор пользователя.
-				if intValue, err := strconv.Atoi(value); err == nil {
-					if key == "vk_ts" {
-						timestamp = intValue
-					} else if key == "vk_app_id" {
-						appId = intValue
-					} else if key == "vk_user_id" {
-						userId = intValue
-					}
-				}
-			}
 			query = append(query, queryParameter{key, value})
 		} else if key == "sign" {
 			// Если ключ равен "sign", то в значении записана подпись параметров
@@ -127,13 +79,7 @@ func VerifyLaunchParams(
 	// В случае, если подпись параметров не удалось найти, либо параметров с
 	// префиксом "vk_" передано не было, мы считаем параметры запуска невалидными.
 	if sign == "" || len(query) == 0 {
-		return nil, errors.New(SignInvalid)
-	}
-
-	// В случае, если требуется проверка на истечения срока годности параметров
-	// запуска, мы требуем наличие параметра "vk_ts" и проверяем его.
-	if expiresIn > 0 && (timestamp == 0 || time.Now().After(time.Unix(int64(timestamp+expiresIn), 0))) {
-		return nil, errors.New(Expired)
+		return errors.New(SignInvalid)
 	}
 
 	// Сортируем параметры запуска по порядку их возрастания.
@@ -164,10 +110,7 @@ func VerifyLaunchParams(
 	hash = strings.Replace(hash, "=", "", -1)
 
 	if sign != hash {
-		return nil, errors.New(SignInvalid)
+		return errors.New(SignInvalid)
 	}
-	return &LaunchParams{
-		UserId: userId,
-		AppId:  appId,
-	}, nil
+	return nil
 }
